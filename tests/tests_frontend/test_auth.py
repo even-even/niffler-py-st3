eed1b540337a86657f8f206db789fda382c96e8d
vyuroshchin <vyuroshchin@sberautotech.ru>
2025-09-15 10:44:15 +0300

    homework2_add_fixture_and_add_more_tests

--- tests/tests_frontend/test_auth.py
+++ tests/tests_frontend/test_auth.py
@@ -5,13 +5,24 @@ from tests.data.filters import INCORRECT_AUTH_PARAMS
 from tests.src.screens.auth_screen import AuthScreen
 
 
+@pytest.mark.usefixtures("open_auth_screen")
+def test_login(page, create_account, browser_type) -> None:
+    allure.dynamic.title(f"Регистрация аккаунта ({browser_type})")
+
+    auth_screen = AuthScreen(page)
+    auth_screen.fill_username(create_account["name"])
+    auth_screen.fill_password(create_account["password"])
+    auth_screen.click_login_button()
+    "Congratulations! You've registered!"
+
+
+@pytest.mark.usefixtures("open_auth_screen")
 @pytest.mark.parametrize("login, password", INCORRECT_AUTH_PARAMS)
 def test_unsuccess_login(page, login, password, browser_type) -> None:
     allure.dynamic.title(f"Регистрация аккаунта с некорректными данными {login}-{password} ({browser_type})")
 
     auth_screen = AuthScreen(page)
 
-    auth_screen.goto_with_awaiting(page, auth_screen.url)
     auth_screen.fill_username(login)
     auth_screen.fill_password(password)
     auth_screen.click_login_button()
